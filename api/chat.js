aef3fa8201811bf88363d4ccfd77ac8c69aab244
Sentinel <micaelalopezyaquelin@gmail.com>
2026-05-17 02:20:30 -0300

    chat.js

--- api/chat.js
+++ api/chat.js
@@ -146,4 +146,4 @@ export default async function handler(req, res) {
     } catch (error) {
         return res.status(500).json({ error: "Fallo crítico controlado en el enrutador." });
     }
-            }
+                        }
